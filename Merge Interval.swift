63337026f6e1c50aef8193217628fba80907b7b4
Varun Rathi <varun.rathi@jeevansathi.com>
2020-07-14 01:32:09 +0530

    Merge interval Arrays:Swift

---
+++ Merge Interval.swift
@@ -0,0 +1,37 @@
+//
+//  Merge Interval.swift
+//  
+//
+//  Created by Varun Rathi on 14/07/20.
+//
+
+import UIKit
+
+class MergeInterval {
+    func getMergedInterval(_ input:[(Int,Int)]) -> [(Int,Int)] {
+        var output = [(Int,Int)]()
+        for i in input {
+            if var lastInterval = output.popLast() {
+                
+                if lastInterval.1 >= i.0 {
+                    lastInterval.1 = i.1
+                    output.append(lastInterval)
+                }
+                else {
+                    output.append(lastInterval)
+                    output.append(i)
+                }
+            }
+            else {
+                output.append((i.0,i.1))
+            }
+        }
+        return output
+    }
+    
+    func test(){
+        let i = [(1, 5), (3, 7), (4, 6), (6, 8),(10, 12),(12, 15)]
+        let r = getMergedInterval(i)
+        print(r)
+    }
+}
--- Sorting/QuickSort.swift
+++ Sorting/QuickSort.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+
 class QuickSort{
     
     // generic method for swaping
